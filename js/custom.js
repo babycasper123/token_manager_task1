8d14a5a28ad10a52f96cd8e9d6df8c9cccb06521
VCH09 <babycasper123@gmail.com>
2017-10-10 17:33:40 +0530

    10-10-2017

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,7 @@
-//Collapse toggle
 
+//Collapse toggle
 $( document ).ready(function() {
     $(".collapse-toggle").click(function(){
         $(this).find("i").toggleClass("fa-angle-down fa-angle-up")
     });
 });
-
-
-// Show/hide Delete on hover
-
-
-// $(document).on('mouseenter', '.child-row', function () {
-//     $(this).find(".mini-delete").toggleClass( "hidden");
-// }).on('mouseleave', '.child-row', function () {
-//     $(this).find(".mini-delete").toggleClass( "hidden");
-// });
